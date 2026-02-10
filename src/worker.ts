c932b3ac1f35c56eed9c564dc7ce4eb592bee89c
Aqib2110 <muhammadaqib2110@gmail.com>
2026-02-10 18:48:04 +0500

    updated worker

--- src/worker.ts
+++ src/worker.ts
@@ -12,9 +12,14 @@ const connection = new Redis({
   port: Number(process.env.REDIS_PORT),
   username: process.env.REDIS_USERNAME,
   password: process.env.REDIS_PASSWORD,
-  tls: {}, 
-  maxRetriesPerRequest: null
+ tls: {
+    rejectUnauthorized: false, 
+  },
+
+  connectTimeout: 10000,
+  maxRetriesPerRequest: null,
 });
+
 
 connection.on('connect', () => console.log('Connected to Aiven Valkey!'));
 connection.on('error', (err) => console.error('Redis error:', err));
